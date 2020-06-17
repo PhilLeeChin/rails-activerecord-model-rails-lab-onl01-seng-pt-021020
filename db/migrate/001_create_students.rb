ef8bbfdfa98c89e1f4a3f36436ba558125717f09
Philip Lee-Chin <phil.leechin@outlook.com>
2020-06-17 14:54:27 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -1,7 +1,7 @@
 class CreateStudents < ActiveRecord::Migration
   def change
     create_table :students do |s|
-      s.string :firt_name
+      s.string :first_name
       s.string :last_name
     end
   end
